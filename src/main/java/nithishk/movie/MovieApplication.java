dda5a6e1dfd22020af07cb3b98d1a3f5c619187d
Nithish Kumar Megarajan <nithishkhw05@gmail.com>
2023-01-28 21:02:38 +0000

    BackEnd API working

--- src/main/java/nithishk/movie/MovieApplication.java
+++ src/main/java/nithishk/movie/MovieApplication.java
@@ -2,7 +2,9 @@ package nithishk.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "nithishk.movie")
 @SpringBootApplication
 public class MovieApplication {
 
